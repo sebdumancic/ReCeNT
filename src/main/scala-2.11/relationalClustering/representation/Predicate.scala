655c394eecd07de071aaab2b0b131b512ffbb7cd
Sebac <sebastijan.dumancic@gmail.com>
2016-02-02 18:22:57 +0100

    predicate commented and role added

--- src/main/scala-2.11/relationalClustering/representation/Predicate.scala
+++ src/main/scala-2.11/relationalClustering/representation/Predicate.scala
@@ -3,19 +3,30 @@ package relationalClustering.representation
 /**
  *  This class implements predicate functionality for finding candidates to combine.
  *  It stores the basic information about the predicate, as well as true groundings
+  *
+  * @constructor creates a predicate with given name and domains
+  * @param name name of the predicate: String
+  * @param domains domains on which the predicate operates: List[Domain]
  *
  * Created by seb on 08.05.15.
  */
 class Predicate(
-               var name: String,
-               val domains: List[Domain]
+               protected var name: String,
+               protected val domains: List[Domain],
+               protected val role: String
                  ) {
 
-  private val trueGroundings: collection.mutable.Set[List[String]] = collection.mutable.Set[List[String]]()
-  private var argumentDeclarations = List[String]()
+  protected val trueGroundings: collection.mutable.Set[List[String]] = collection.mutable.Set[List[String]]()
+  protected var argumentDeclarations = List[String]()
 
+  /** Returns the set of true groundings
+    * @return Set[List[String] ]*/
   def getTrueGroundings: collection.mutable.Set[List[String]] = { trueGroundings }
 
+  /** Sets the true assignment of the predicate for the given arguments
+    *
+    * @param ground list of arguments: List[String]
+    * */
   def setTrueGrounding(ground: List[String]) = {
     require( ground.length == arity, s"Wrong number of arguments [$name($ground)] -- ${ground.length} != $arity")
     trueGroundings += ground
@@ -24,27 +35,77 @@ class Predicate(
     ground.zipWithIndex.foreach( elem => domains(elem._2).addElement(elem._1))
   }
 
+  /** Sets the false assignment of the predicate fpr the given arguments
+    *
+    * @param ground list of arguments: List[String]
+    * */
   def removeTrueGrounding(ground: List[String]) = {
     require( ground.size == arity, "Wrong number of arguments")
     trueGroundings -= ground
   }
 
-  def arity: Int = { domains.length }
+  /** Returns the arity of the predicate
+    *
+    * @return Int
+    * */
+  def arity: Int = {
+    domains.length
+  }
 
-  def getName: String = { name }
+  /** Returns the name of the predicate
+    * @return name: String
+    * */
+  def getName: String = {
+    name
+  }
 
-  def getDomains: List[String] = { domains.map( _.getName ) }
+  /** Returns the domains of the predicate
+    *
+    * @return List[String]
+    * */
+  def getDomains: List[String] = {
+    domains.map( _.getName )
+  }
 
-  def getDomainObjects = { domains }
-  
+  /** Returns the Domains objects of the predicate
+    * @return List[Domain]
+    * */
+  def getDomainObjects = {
+    domains
+  }
+
+  /** Checks whether given arguments evaluates to true
+    *
+    * @param grounding list of arguments: List[String]
+    * @return Boolean
+    * */
   def isTrue(grounding: List[String]) = {
     trueGroundings.contains(grounding)
   }
 
   override def toString = getName + "(" + getDomains.mkString(",") + ")"
 
-  def setName(newName: String) = { name = newName}
+  /** Sets a new name for the predicate
+    *
+    * @param newName new name to be set: String
+    * */
+  def setName(newName: String) = {
+    name = newName
+  }
 
-  def setDeclarations(declarations: List[String]) = { argumentDeclarations = declarations }
+  /** Sets the declarations of the arguments */
+  def setDeclarations(declarations: List[String]) = {
+    argumentDeclarations = declarations
+  }
+
+  /** Returns the role of the argument at the specified position
+    *
+    * @param position position of the arguments: Int
+    * */
   def getArgumentRole(position: Int) = { argumentDeclarations(position) }
+
+  /** Returns the role of the predicate*/
+  def getRole = {
+    role
+  }
 }
