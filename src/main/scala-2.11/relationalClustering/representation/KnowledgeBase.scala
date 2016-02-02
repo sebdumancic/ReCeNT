999fc3e2cb4c238d24b92ec4e6df4aaa6482c8ea
Sebac <sebastijan.dumancic@gmail.com>
2016-02-02 19:35:34 +0100

    KnowledgeBase commented, adds the role of the predicate when created

--- src/main/scala-2.11/relationalClustering/representation/KnowledgeBase.scala
+++ src/main/scala-2.11/relationalClustering/representation/KnowledgeBase.scala
@@ -2,61 +2,100 @@ package relationalClustering.representation
 
 import java.io.{BufferedWriter, FileWriter}
 
-import relationalClustering.utils.Helper
+import relationalClustering.utils.{PredicateDeclarations, Helper}
 
 
 /**
  *  This class implements a knowledge base managing predicates and their true groundings
  *
+  * @constructor creates a knowledge base from the set of facts, definitions and declarations of the predicates
+  * @param databases a list of databases containing logical facts: Set[String]
+  * @param header definitions of the predicates: String
+  * @param predicateDeclarations declarations of the predicates: [[PredicateDeclarations]]
  * Created by seb on 08.05.15.
  */
 
 
-class KnowledgeBase(
-                   val databases: Seq[String],
-                   val header: String
-                     ) {
+class KnowledgeBase(private val databases: Seq[String],
+                   private val header: String,
+                   private val predicateDeclarations: PredicateDeclarations
+                   ) {
 
   private val predicates = collection.mutable.Map[String, Predicate]()
-  val predicate_regex = """(.*?)\((.*?)\).*""".r
+  private val predicate_regex = """(.*?)\((.*?)\).*""".r
   private val domainObjects = collection.mutable.Map[String, Domain]()
 
   constructPredicates()
   processDatabases()
 
+  /** Returns the domain with speficified name
+    *
+    * @param name name of the domain: [[String]]
+    * @return Domain: [[Domain]]
+    * */
   def getDomainObject(name: String) = {
     if (!domainObjects.contains(name)) { domainObjects(name) = new Domain(name)}
     domainObjects(name)
   }
 
+  /** Creates a predicate with the given name and domains
+    *
+    * @param name name of the predicate: [[String]]
+    * @param domains list of domain a predicate operates on: [[List]]
+    * */
   def createPredicate(name: String, domains: List[String]) = {
-    predicates(name) = new Predicate(name.trim, domains.map( x => getDomainObject(x.trim) ))
+    predicates(name) = new Predicate(name.trim, domains.map( x => getDomainObject(x.trim) ), predicateDeclarations.getPredicateRole(name.trim))
   }
 
+  /** Adds predicate to the collection of predicate
+    *
+    * @param pred predicate to be added: [[Predicate]]
+    * */
   def addPredicate(pred: Predicate) = {
     predicates(pred.getName) = pred
   }
 
+  /** Returns a predicate with a given name
+    *
+    * @param name name of the predicate: [[String]]
+    * @return [[Predicate]]
+    * */
   def getPredicate(name: String) = {
     require(predicates contains name, s"Requesting non-existing predicate $name!")
     predicates(name)
   }
 
+  /** Returns the list of all predicate names
+    *
+    * @return [[List]]
+    * */
   def getPredicateNames = {
     predicates.keys.toList
   }
 
+  /** Adds an object to the domain
+    *
+    * @param domain name of the domain: [[String]]
+    * @param domainObject objects name: [[String]]
+    * */
   def addToDomain(domain: String, domainObject: String) = {
     getDomainObject(domain.trim).addElement(domainObject)
   }
 
+  /** Returns the domain with a specified name
+    *
+    * @param domain name of the domain: [[String]]
+    * @return [[Domain]]
+    * */
   def getDomain(domain: String) = {
     require(domainObjects contains domain, s"Accessing a non-existing domain $domain")
     domainObjects(domain)
   }
 
+  /** Returns the set of all domain objects*/
   def getAllDomains = { domainObjects }
 
+  /** Constructs the predicates specified in the header*/
   private def constructPredicates() = {
     for(predicate <- header.split("\n")) {
       if (predicate.length > 2 && !predicate.contains("//")) {
@@ -67,6 +106,12 @@ class KnowledgeBase(
     }
   }
 
+  /** Adds a true grounding
+    *
+    * @param grounding a grounding of the predicate predicate(arguments): [[String]]
+    *
+    * Each encountered object is also added to its domain
+    * */
   def addTrueGrounding(grounding: String) = {
     val predicate_regex(predicate_name, predicate_arguments) = grounding
     val argument_list = predicate_arguments.split(",").toList
@@ -83,15 +128,19 @@ class KnowledgeBase(
     line.replace("_target", "").replace("_source", "")
   }
 
+  /** Read a specified database
+    *
+    * @param db a filepath to the database to read: [[String]]
+    * */
   private def processDatabase(db: String) = {
     for(line <- Helper.readFile(db)) {
       if (line.length > 2) {
         addTrueGrounding(cleanForm(line))
       }
-
     }
   }
 
+  /** Reads all specified databases */
   private def processDatabases() = {
     for(db <- databases) {
       processDatabase(db)
