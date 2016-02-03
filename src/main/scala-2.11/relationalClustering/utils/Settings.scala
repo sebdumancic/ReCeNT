eb7784849c28c6d7d852fc1fdaae1d8a105b7a1a
Sebac <sebastijan.dumancic@gmail.com>
2016-02-03 13:42:50 +0100

    settings file

---
+++ src/main/scala-2.11/relationalClustering/utils/Settings.scala
@@ -0,0 +1,17 @@
+package relationalClustering.utils
+
+/**
+  * Created by seb on 03.02.16.
+  */
+object Settings {
+
+  /** PREDICATE ROLES */
+  val ROLE_ANNOTATION = "annotation"
+  val ROLE_ATTRIBUTE = "attribute"
+  val ROLE_HYPEREDGE = "hyperedge"
+
+  /** ARGUMENT TYPES */
+  val ARG_TYPE_ATTRIBUTE = "attribute"
+  val ARG_TYPE_NUMBER = "number"
+  val ARG_TYPE_NAME = "name"
+}
