2fcd673cb635c1b828468a536b3051787abb8c72
Sebac <sebastijan.dumancic@gmail.com>
2016-02-03 13:11:09 +0100

    added attribute value accessors; commented code

--- src/main/scala-2.11/relationalClustering/neighbourhood/NeighbourhoodGraph.scala
+++ src/main/scala-2.11/relationalClustering/neighbourhood/NeighbourhoodGraph.scala
@@ -3,42 +3,56 @@ package relationalClustering.neighbourhood
 import relationalClustering.representation.{Predicate, KnowledgeBase}
 
 /**
- * Implements neighbourhood graph functionality that is used to estimate indirect links measure within similarity measure
- *
- * implements a rooted graph, where a root is an object of interest
- *
- * Created by seb on 16.10.15.
+  * Implements neighbourhood graph functionality that is used to estimate indirect links measure within similarity measure
+  *
+  * @constructor creates a neighbourhood graph for a given object name and domain, with the given depth and knowledge base
+  *
+  * @param rootObject name of the root object: [[String]]
+  * @param domain domain of the root element: [[String]]
+  * @param depth depth of the neighbourhood graph: [[Int]]
+  * @param kBase knowledge base to construct: [[KnowledgeBase]]
+  * @param nodeRepo node repository: [[NodeRepository]]
+  *
+  * Created by seb on 16.10.15.
  */
-class NeighbourhoodGraph(val rootObject: String,
-                         val domain: String,
-                         val depth: Int,
-                         val kBase: KnowledgeBase) {
+class NeighbourhoodGraph(protected val rootObject: String,
+                         protected val domain: String,
+                         protected val depth: Int,
+                         protected val kBase: KnowledgeBase,
+                         protected val nodeRepo: NodeRepository) {
 
-  val root = new Node(rootObject, domain)
-  val nodes = collection.mutable.Map[String, Node]() //collection of all connected objects for easier manipulation
-  nodes(rootObject) = getRoot
+  val root = nodeRepo.getNode(rootObject, domain) //new Node(rootObject, domain)
   construct()
 
+  /** Return the knowledge base of an NG*/
   def getKnowledgeBase = {
     kBase
   }
 
+  /** Returns the domain of the root element */
   def getRootDomain = {
     domain
   }
 
+  /** Return the root element */
   def getRoot = {
     root
   }
 
+  /** Return the depth of the NG */
   def getMaxDepth = {
     depth
   }
 
-  def addNode(node: Node) = {
-    nodes(node.getEntity) = node
+  /** Returns the root elements attributes (including annotations)*/
+  def getRootAttributes = {
+    getRoot.getAttributeValuePairs ++ getRoot.getAnnotations.map( x => new Tuple2(x, "true"))
   }
 
+  /** Constructs all the edges for the node, by traversing all the hyperedge predicates
+    *
+    * @param node node to create edges for: Node
+    * */
   private def constructNodeEdges(node: Node) = {
     //includes only relationship into account here
     getKnowledgeBase.getPredicateNames.map(kBase.getPredicate).filter(_.arity > 1).filter(_.getDomains.contains(node.getDomain)).foreach(pred => {
@@ -46,6 +60,11 @@ class NeighbourhoodGraph(val rootObject: String,
     })
   }
 
+  /** Construct the edges for the node, given the edge type
+    *
+    * @param node node to create edges for: Node
+    * @param predicate type of the relation: Predicate
+    * */
   private def constructEdgeGivenPredicate(node: Node, predicate: Predicate) = {
 
     //iterate over potential bindings
@@ -56,19 +75,16 @@ class NeighbourhoodGraph(val rootObject: String,
         ground.zipWithIndex.filterNot(_._1 == node.getEntity).foreach(child => {
           //filterNot - no recursive links allowed
 
-          //if node does not exits, create it first
-          if (!nodes.contains(child._1)) {
-            addNode(new Node(child._1, predicate.getDomains(child._2)))
-          }
-
           //connect the nodes
-          node.addChild(nodes(child._1), predicate, focusNodePosition)
-          nodes(child._1).addParent(node, predicate, focusNodePosition)
+          val childNode = nodeRepo.getNode(child._1, predicate.getDomains(child._2))
+          node.addChild(childNode, predicate, focusNodePosition)
+          childNode.addParent(node, predicate, focusNodePosition)
         })
       })
     })
   }
 
+  /** Constructs the Neighbourhood graph */
   def construct() = {
     var frontier = List[Node](getRoot)
     var currentDepth = 0
@@ -91,13 +107,12 @@ class NeighbourhoodGraph(val rootObject: String,
     }
   }
 
-  /*
-    collects the information about types encountered on each level of graph
-
-    :returns: a Map[Int,Map[String1, List[String]]] (Int -> Level starting with 0, String1 -> domains, List => list of found objects)
-   */
-  def collectTypeInformation() = {
-    //construct()
+  /** Collects the vertex identity information in the Neighbourhood graph
+    *
+    * @return Map[Int, Map[String, List[String] ] ] (level -> domain -> list of objects)
+    *
+    * */
+  def collectVertexIdentity() = {
     val resultSummary = collection.mutable.Map[Int,collection.mutable.Map[String,List[String]]]()
     var currentLevel = 0
     var frontier = List[Node](getRoot)
@@ -127,24 +142,38 @@ class NeighbourhoodGraph(val rootObject: String,
     resultSummary
   }
 
-  def collectTypeInformation(level: Int): Map[String, List[String]] = {
-    val allInformation = collectTypeInformation(); allInformation(level).toMap
+  /** Collects the vertex identity information on a certain level
+    *
+    * @param level level of interest: Int
+    * */
+  def collectVertexIdentity(level: Int): Map[String, List[String]] = {
+    val allInformation = collectVertexIdentity(); allInformation(level).toMap
   }
 
+  /** Return the root predicates (relations) */
   def getRootPredicates = {
     getRoot.getChildEdges.map(_.getPredicate)
+  }
+
+  /** Returns the edge type of the root element (including multiplicity) */
+  def getRootEdgeTypes = {
+    getRoot.getChildEdges.toList.map( _.getEdgeType )
   }
 
   override def toString = {
     getRoot.asString("")
   }
 
+  /** Collects edge types over levels
+    *
+    * @return Map[Int, List[String] ]; depth -> list of edge types
+    * */
   def getEdgeDistribution = {
     val levelWise = collection.mutable.Map[Int, List[String]]() // depth -> List[Edge distribution]
 
     var currentDepth = 0
-    var frontier = List[Node](getRoot)
-    var newFrontier = List[Node]()
+    var frontier = Set[Node](getRoot)
+    var newFrontier = Set[Node]()
 
     while (currentDepth <= getMaxDepth) {
 
@@ -155,132 +184,53 @@ class NeighbourhoodGraph(val rootObject: String,
 
         //expand new frontier
         cNode.getChildNodes.foreach(child => {
-          newFrontier = newFrontier :+ child
+          newFrontier = newFrontier + child
         })
       })
 
       frontier = newFrontier.map(x => x)
-      newFrontier = List[Node]()
+      newFrontier = Set[Node]()
       currentDepth = currentDepth + 1
     }
 
     levelWise.toMap
   }
-}
 
+  /** Collects the attribute value information, as well as annotations, per level and vertex type
+    *
+    * @return Map[Int, Map[String, List[(String,String)] ] ] level -> type -> list of attribute-values
+    * */
+  def getAttributeValueDistribution = {
+    val returnData = collection.mutable.Map[Int, Map[String, List[(String,String)]]]()
 
-class Node(val entity: String,
-           val domain: String) {
+    var currentDepth = 0
+    var frontier = Set[Node](getRoot)
+    var newFrontier = Set[Node]()
 
-  var parents = Set[Edge]()
-  var children = Set[Edge]()
+    while (currentDepth <= getMaxDepth) {
+      val levelContent = collection.mutable.Map[String, List[(String,String)]]()
+      val domains = frontier.map( _.getDomain )
 
-  def getDomain = {
-    domain
-  }
+      //collecting over nodes at current level
+      domains.foreach( dom => {
+        levelContent(dom) = frontier.filter( _.getDomain == dom ).foldLeft(List[(String,String)]())( (acc, cNode) => {
+          //expand the frontier
+          cNode.getChildNodes.foreach(child => {
+            newFrontier = newFrontier + child
+          })
 
-  def getEntity = {
-    entity
-  }
+          acc ++ cNode.getAttributeValuePairs.toList ++ cNode.getAnnotations.toList.map( x => new Tuple2(x, "true"))
+        })
+      })
 
-  def addParent(parent: Node, predicate: Predicate, parentPosition: Int) = {
-    if (parent.getEntity != getEntity || parent.getDomain != getDomain) {
-      parents = parents + new Edge(parent, this, predicate, parentPosition)
+      //put content in returnData
+      returnData(currentDepth) = levelContent.toMap
+
+      frontier = newFrontier.map( x => x )
+      newFrontier = Set[Node]()
+      currentDepth = currentDepth + 1
     }
-  }
 
-  def getParentNodes = {
-    parents.map(_.getParent)
-  }
-
-  def getParentEdges = {
-    parents
-  }
-
-  def addChild(child: Node, predicate: Predicate, parentPosition: Int) = {
-    if ((child.getEntity != getEntity || child.getDomain != getDomain) && !getParentEdges.map(_.getParent).contains(child)) {
-      children = children + new Edge(this, child, predicate, parentPosition)
-    }
-  } //takes care of cycle edges
-
-  def getChildEdges = {
-    children
-  }
-
-  def getChildRelationships = {
-    children.map(_.getPredicate)
-  }
-
-  def getChildNodes = {
-    children.map(_.getChild)
-  }
-
-  def getTypedChildren(domainType: String) = {
-    children.filter(x => {
-      x.getChild.getDomain == domainType
-    })
-  }
-
-  def getPredicateChildren(predicate: Predicate) = {
-    children.filter(_.getPredicate == predicate).map(_.getChild)
-  }
-
-  def asString(prefix: String): String = {
-    s"$getEntity[$getDomain]\n" +
-      prefix + "|---" + getChildNodes.map(_.asString(s"$prefix|   ")).mkString(s"\n$prefix|---")
-  }
-
-  override def toString = {
-    s"$getEntity[$getDomain]"
-  }
-
-  override def hashCode = {
-    toString.hashCode
-  }
-
-  override def equals(other: Any) = other match {
-    case other: Node => hashCode == other.hashCode
-    case _ => false
-  }
-
-}
-
-
-class Edge(val parentNode: Node,
-           val childNode: Node,
-           val relationshipPredicate: Predicate,
-           val parentPositionInGrounding: Int) {
-
-  def getChild = {
-    childNode
-  }
-
-  def getParent = {
-    parentNode
-  }
-
-  def getPredicate = {
-    relationshipPredicate
-  }
-
-  def getParentPosition = {
-    parentPositionInGrounding
-  }
-
-  def getEdgeType = {
-    s"${getPredicate.getName}$getParentPosition"
-  }
-
-  override def toString = {
-    s"$getParent --[${getPredicate.getName}],[$getParentPosition]--> $getChild"
-  }
-
-  override def equals(other: Any) = other match {
-    case other: Edge => hashCode == other.hashCode
-    case _ => false
-  }
-
-  override def hashCode = {
-    toString.hashCode
+    returnData.toMap
   }
 }
