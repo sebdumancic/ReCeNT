fc8e19d9b0ef8ea18d74bf77258a7ddbff9b101a
Sebac <sebastijan.dumancic@gmail.com>
2016-02-03 17:44:47 +0100

    abstract bag comparison class added

---
+++ src/main/scala-2.11/relationalClustering/bagComparison/AbstractBagCompare.scala
@@ -0,0 +1,20 @@
+package relationalClustering.bagComparison
+
+/**
+  * Created by seb on 03.02.16.
+  */
+abstract class AbstractBagCompare(protected val identity: String) {
+
+
+  /** Returns the identity of the bag compare function */
+  def name = {
+    identity
+  }
+
+  /** Takes two bags and compares them
+    *
+    * @param bag1 list of elements of the first bag
+    * @param bag2 list of elements of the second bag
+    * */
+  def compareBags(bag1: List[String], bag2: List[String]): Double
+}
