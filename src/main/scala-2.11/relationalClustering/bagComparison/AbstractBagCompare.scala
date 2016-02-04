240e9d70653ca00085715ccb38522506f5404f27
Sebac <sebastijan.dumancic@gmail.com>
2016-02-04 14:59:00 +0100

    added histogram construction methods to AbstractBagCompare

--- src/main/scala-2.11/relationalClustering/bagComparison/AbstractBagCompare.scala
+++ src/main/scala-2.11/relationalClustering/bagComparison/AbstractBagCompare.scala
@@ -20,4 +20,38 @@ abstract class AbstractBagCompare(protected val identity: String) {
 
   /** Should measure be normalized and inverted to be turned into a similarity measure */
   def needsToBeInverted : Boolean
+
+  /** Extract the union of all elements in two bags; in case there are no element, put a fake element in a bag to avoid problems later
+    *
+    * @param bag1 list of all elements in bag one
+    * @param bag2 list of all elements in bag two
+    * @return list of all common elements
+    * */
+  protected def commonElements(bag1: List[String], bag2: List[String]) = {
+    val distinctElements = bag1.toSet.union(bag2.toSet)
+
+    distinctElements.isEmpty match {
+      case true => List[String]("fakeElement")
+      case false => distinctElements.toList
+    }
+  }
+
+  /** Turns bag into a normalized histogram
+    *
+    * @param bag bag to be turned into histogram
+    * @param histogramElements ordered elements in the histogram
+    * @return list[Double]
+    * */
+  protected def bagToHistogram(bag: List[String], histogramElements: List[String]) = {
+    normalizeBySum(histogramElements.map(x => bag.count( _ == x))).map(x => if (x.isNaN) 0.0 else x)
+  }
+
+  /** Normalizing histogram by the total number of elements
+    *
+    * @param histogram ordered list of element counts
+    * */
+  protected def normalizeBySum(histogram: List[Int]) = {
+    val normConstant = histogram.sum
+    histogram.map( _.toDouble / normConstant)
+  }
 }
