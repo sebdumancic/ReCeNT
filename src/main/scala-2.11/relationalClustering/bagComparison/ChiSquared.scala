e7201b8f13247aeb3d78a2a77c4879da709905f2
Sebac <sebastijan.dumancic@gmail.com>
2016-02-04 15:12:45 +0100

    chi squared distance implemented

---
+++ src/main/scala-2.11/relationalClustering/bagComparison/ChiSquared.scala
@@ -0,0 +1,16 @@
+package relationalClustering.bagComparison
+
+/**
+  * Implements a Chi Squared distance between two normalized histograms
+  * Created by seb on 04.02.16.
+  */
+class ChiSquared(override protected val identity: String) extends AbstractBagCompare(identity) {
+
+  def needsToBeInverted = {
+    true
+  }
+
+  protected def similarity(hist1: List[Double], hist2: List[Double]) = {
+    hist1.zip(hist2).map( x => math.pow(x._1 - x._2, 2.0)/(x._1 + x._2)).map( x => if (x.isNaN) 0.0 else x).sum
+  }
+}
