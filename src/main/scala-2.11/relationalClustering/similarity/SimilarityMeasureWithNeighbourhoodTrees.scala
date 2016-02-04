59e53020a93f101020df053e010c97b225cac047
Sebac <sebastijan.dumancic@gmail.com>
2016-02-04 14:36:23 +0100

    abstract class with neighbourhood graph cache integrated

---
+++ src/main/scala-2.11/relationalClustering/similarity/SimilarityMeasureWithNeighbourhoodTrees.scala
@@ -0,0 +1,25 @@
+package relationalClustering.similarity
+
+import relationalClustering.neighbourhood.NeighbourhoodGraph
+import relationalClustering.representation.KnowledgeBase
+
+/**
+  * Created by seb on 04.02.16.
+  */
+abstract class SimilarityMeasureWithNeighbourhoodTrees(override protected val knowledgeBase: KnowledgeBase,
+                                                       override protected val depth: Int) extends AbstractSimilarityMeasure(knowledgeBase, depth) {
+
+  /** caches neighbourhood graphs to be built only once
+    * key (objectName, domain)
+    * */
+  protected val neighbourhoodGraphCache = collection.mutable.Map[(String,String), NeighbourhoodGraph]()
+
+  protected def getNeighbourhoodGraph(objectName: String, domain: String) = {
+    val keytoUse = new Tuple2(objectName, domain)
+    if (!neighbourhoodGraphCache.contains(keytoUse)) {
+      neighbourhoodGraphCache(keytoUse) = new NeighbourhoodGraph(objectName, domain, getDepth, getKB, getRepo)
+    }
+    neighbourhoodGraphCache(keytoUse)
+  }
+
+}
