5f34242eae250a6734a37239155150c8ce2414e0
Sebac <sebastijan.dumancic@gmail.com>
2016-02-08 17:16:44 +0100

    compatible version

---
+++ src/main/scala-2.11/relationalClustering/similarity/SimilarityNTv2.scala
@@ -0,0 +1,29 @@
+package relationalClustering.similarity
+
+import relationalClustering.bagComparison.AbstractBagCompare
+import relationalClustering.neighbourhood.NeighbourhoodGraph
+import relationalClustering.representation.KnowledgeBase
+
+/**
+  * Created by seb on 08.02.16.
+  */
+class SimilarityNTv2(override protected val knowledgeBase: KnowledgeBase,
+                     override protected val depth: Int,
+                     override protected val weights: List[Double],
+                     override protected val bagCompare: AbstractBagCompare) extends SimilarityNeighbourhoodTrees(knowledgeBase, depth, weights, bagCompare) {
+
+  override protected def attributeNeighbourhoodSimilarity(ng1: NeighbourhoodGraph, ng2: NeighbourhoodGraph): Double = {
+    val firstGraph = ng1.getAttributeValueDistribution(0)
+    val secondGraph = ng2.getAttributeValueDistribution(0)
+
+    val firstAttributes = firstGraph.foldLeft(List[(String,String)]())( (acc, dom) => {
+      acc ::: dom._2
+    })
+    val secondAttributes = secondGraph.foldLeft(List[(String,String)]())( (acc, dom) => {
+      acc ::: dom._2
+    })
+
+    bagCompare.compareBags(firstAttributes.map( x => s"${x._1}${x._2}"), secondAttributes.map(x => s"${x._1}${x._2}"))
+  }
+
+}
