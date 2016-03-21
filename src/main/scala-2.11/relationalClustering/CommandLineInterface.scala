03be84ce98fd902263b4c2f791eb037535a211b3
Sebac <sebastijan.dumancic@gmail.com>
2016-03-21 14:35:59 +0100

    added majority class evaluation

--- src/main/scala-2.11/relationalClustering/CommandLineInterface.scala
+++ src/main/scala-2.11/relationalClustering/CommandLineInterface.scala
@@ -3,7 +3,7 @@ package relationalClustering
 import org.clapper.argot.ArgotParser
 import relationalClustering.bagComparison.bagCombination.{IntersectionCombination, UnionCombination}
 import relationalClustering.bagComparison.{ChiSquaredDistance, MaximumSimilarity, MinimumSimilarity, UnionBagSimilarity}
-import relationalClustering.clustering.evaluation.{AdjustedRandIndex, AverageIntraClusterSimilarity, LabelsContainer}
+import relationalClustering.clustering.evaluation.{AdjustedRandIndex, AverageIntraClusterSimilarity, LabelsContainer, MajorityClass}
 import relationalClustering.clustering.{Hierarchical, Spectral}
 import relationalClustering.representation.domain.KnowledgeBase
 import relationalClustering.similarity.{HSAG, NevilleSimilarityMeasure, SimilarityNTv2, SimilarityNeighbourhoodTrees}
@@ -33,7 +33,7 @@ object CommandLineInterface {
   val linkage = parser.option[String](List("linkage"), "[average|complete|ward]", "linkage for hierarchical clustering")
   val validate = parser.flag[Boolean](List("validate"), "should validation be performed")
   val labels = parser.option[String](List("labels"), "file path to the labels", "labels for the query objects")
-  val valMethod = parser.option[String](List("validationMethod"), "[ARI|RI|intraCluster]", "cluster validation method")
+  val valMethod = parser.option[String](List("validationMethod"), "[ARI|RI|intraCluster|majorityClass]", "cluster validation method")
   val useLocalRepository = parser.flag[Boolean](List("localRepo"), "should NodeRepository be constructed locally for each NeighbourhoodGraph, or one globally shared")
 
 
@@ -117,6 +117,9 @@ object CommandLineInterface {
         case "intraCluster" =>
           val validator = new AverageIntraClusterSimilarity()
           println(s"${valMethod.value.getOrElse("ARI")} score: ${validator.validate(clusters, globalElementOrder, globalFilename)}")
+        case "majorityClass" =>
+          val validator = new MajorityClass()
+          println(s"${valMethod.value.get} score: ${validator.validate(clusters, labContainer)}")
       }
 
     }
---
+++ src/main/scala-2.11/relationalClustering/clustering/evaluation/MajorityClass.scala
@@ -0,0 +1,34 @@
+package relationalClustering.clustering.evaluation
+
+/**
+  * Created by seb on 21.03.16.
+  */
+class MajorityClass extends AbstractEvaluatorWithLabels("./tmp") {
+
+  /** Each examples is assigned with a majority label in a cluster
+    *
+    * @param clusters clustering
+    * @param labels labels of the examples
+    * @return accuracy of such assignment (#correctly assigned labels / #elements)
+    * */
+  def validate(clusters: Set[List[String]], labels: LabelsContainer) = {
+
+    clusters.map( clust => new Tuple2(clust, AssignMajorityClass(clust, labels))).foldLeft(Set[(String,String)]())( (acc, cl) => {
+      acc ++ cl._1.map( el => new Tuple2(el, cl._2))
+    }).count( elemCl => elemCl._2 == labels.getLabel(elemCl._1)).toDouble/clusters.map( _.size).sum.toDouble
+  }
+
+  /** Returns the majority class in a cluster
+    *
+    * @param cluster a list of element names in a cluster
+    * @param labels label container
+    * */
+  protected def AssignMajorityClass(cluster: List[String], labels: LabelsContainer) = {
+    val labs = cluster.map( ex => labels.getLabel(ex))
+    val counts = labs.distinct.map( lab => labs.count( _ == lab))
+
+    labs.zip(counts).maxBy(_._2)._1
+  }
+
+
+}
