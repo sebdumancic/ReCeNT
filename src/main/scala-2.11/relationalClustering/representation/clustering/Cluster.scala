97c10420ba675b8977fc7390f464ffc4a8a67747
sdumancic <sebastijan.dumancic@gmail.com>
2016-06-07 11:46:02 +0200

    abstract cluster class

---
+++ src/main/scala-2.11/relationalClustering/representation/clustering/Cluster.scala
@@ -0,0 +1,28 @@
+package relationalClustering.representation.clustering
+
+import relationalClustering.bagComparison.AbstractBagComparison
+import relationalClustering.bagComparison.bagCombination.AbstractBagCombine
+import relationalClustering.neighbourhood.NeighbourhoodGraph
+
+
+/** Implements cluster functionality (container of instances)
+  * Created by seb on 17.03.16.
+  */
+abstract class Cluster(protected val types: List[String],
+                       protected val clusterName: String,
+                       protected val instances: Set[List[String]]) {
+
+  /** Returns the cluster type
+    * */
+  def getTypes = {
+    types
+  }
+
+  /** Returns cluster name
+    * */
+  def getClusterName = {
+    clusterName
+  }
+
+  def distance(nt: NeighbourhoodGraph, weights: List[Double], bagCompare: AbstractBagComparison, bagCombine: AbstractBagCombine): Double
+}
