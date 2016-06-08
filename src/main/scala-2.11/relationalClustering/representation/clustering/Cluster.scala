bdd11a84029cc738d04cfaeeb49ae5af53851988
sdumancic <sebastijan.dumancic@gmail.com>
2016-06-08 11:49:15 +0200

    added printing options for cluster

--- src/main/scala-2.11/relationalClustering/representation/clustering/Cluster.scala
+++ src/main/scala-2.11/relationalClustering/representation/clustering/Cluster.scala
@@ -7,26 +7,32 @@ import relationalClustering.neighbourhood.NeighbourhoodGraph
   * Created by seb on 17.03.16.
   */
 class Cluster(protected val types: List[String],
-                       protected val clusterName: String,
-                       protected val instances: Set[List[String]],
-                       protected val ntRepo: Map[(String,String), NeighbourhoodGraph]) {
+              protected val clusterName: String,
+              protected val instances: Set[List[String]],
+              protected val ntRepo: Map[(String,String), NeighbourhoodGraph]) {
 
-  /** Returns the cluster type
-    * */
+  /** Returns the cluster type */
   def getTypes = {
     types
   }
 
-  /** Returns cluster name
-    * */
+  /** Returns cluster name */
   def getClusterName = {
     clusterName
   }
 
-  /** Returns instance names
-    * */
+  /** Returns instance names */
   def getInstances = {
     instances
+  }
+
+  /** Returns the definition of a predicate representing the cluster */
+  def getDefinition = {
+    s"$getClusterName(${getTypes.mkString(",")})"
+  }
+
+  def getClusterFacts = {
+    getInstances.map(inst => s"$getClusterName(${inst.mkString(",")})")
   }
 
   /** Returns a set of neighbourhood trees for the given instance*/
