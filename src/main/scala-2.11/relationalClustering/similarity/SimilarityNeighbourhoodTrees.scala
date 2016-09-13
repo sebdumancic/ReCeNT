395737c45db8371b81c05d4f0216c0c2f1e69cdb
sdumancic <sebastijan.dumancic@gmail.com>
2016-09-13 10:45:31 +0200

    if no normalization constant calculated, takes 1.0

--- src/main/scala-2.11/relationalClustering/similarity/SimilarityNeighbourhoodTrees.scala
+++ src/main/scala-2.11/relationalClustering/similarity/SimilarityNeighbourhoodTrees.scala
@@ -93,9 +93,13 @@ class SimilarityNeighbourhoodTrees(override protected val knowledgeBase: Knowled
     )
 
     weights.zipWithIndex.filter( _._1 > 0.0).foldLeft(0.0)( (acc, w) => {
-      val norm = objectsNormConstants(w._2) == 0.0 match {
-        case true => 1.0
-        case false => objectsNormConstants(w._2)
+      val norm = objectsNormConstants.contains(w._2) match {
+        case false => 1.0
+        case true =>
+          objectsNormConstants(w._2) == 0.0 match {
+            case true => 1.0
+            case false => objectsNormConstants(w._2)
+          }
       }
       acc + w._1 * (functionsWithNorm(w._2)._1 match {
         case true => 1.0 - (functionsWithNorm(w._2)._2(nt1, nt2)/norm)
